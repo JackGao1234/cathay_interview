5fd2c47b6f4f267a6403f9a1372fd0a10ffc3406
Jack Kao <jackgao1234@gmail.com>
2021-07-17 15:25:34 +0800

    add: ans_3 about merging all 5 real estate data

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 
 if __name__ == "__main__":
-    # ans_2
+    # ans_2: the five cities' real estate data
     df_a = pd.read_csv(r"raw_data\a_lvr_land_a.csv")
     df_b = pd.read_csv(r"raw_data\b_lvr_land_a.csv")
     df_e = pd.read_csv(r"raw_data\e_lvr_land_a.csv")
@@ -15,3 +15,8 @@ if __name__ == "__main__":
     print(df_e.shape)
     print(df_f.shape)
     print(df_h.shape)
+
+    # ans_3: Merge to 1 df
+    frames = [df_a, df_b, df_e, df_f, df_h]
+    df_all = pd.concat(frames)
+    print(f"df_all shape: {df_all.shape}")
